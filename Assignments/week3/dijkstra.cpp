9b64720635c6471c65db518bd2bbbebe3f456eb0
Yevgeny York <yy@ixi.ru>
2017-09-17 19:06:47 +0300

    Finalize the assignment. Print average open shortest path for 50 paths in graph with 50 verticies, 0.2/0.4 dencity and 1-10 distance weights.

--- Assignments/week3/dijkstra.cpp
+++ Assignments/week3/dijkstra.cpp
@@ -19,45 +19,63 @@ public:
 	~Graph();
 	Graph(unsigned int size=10, float density=0.1, W range=10);
 
-	void ospDijkstra(int);
+	void ospDijkstra(int src); // src is the starting point
+	W ospAverage(const unsigned int num); // num is the number of paths which are
+	// involved in average osp calculation
+
 	void printGraph();
 	void printSolution(W dist[], int src);
 	void saveSolution(W dist[], int src);
 
-	int getSize() { return mSize; };
+	int getSize()
+	{
+		return mSize;
+	};
 	W minDistance(W dist[], bool sptSet[]);
 
 private:
 	// "W" means the distance weight, 0 means - no edge
-	W **mGraph; 		// connectivity matrix
+	W **mGraph;		// connectivity matrix
 	W **mOSP;		// open shortest path matrix
 	const int mSize;
 	const int mDensity;
 	const W mRange;
 };
 
-// generate random values for density and distanse weight
+// generate random values for density and distanse 
+// weight calculations
 inline int probe()
 {
 	return (rand() % 100);
 }
+// function to test probe()
+// it prints random sequence for manual checking
+inline void testprobe(int num)
+{
+	cout << "Random sequence testing..." << endl;
+	for (int i=0; i < num; ++i)
+		cout << probe() << " ";
+	cout << endl;
+}
+// generic distance weight generating
+// it suits for signed and unsigned numeric types
+// i.e. int, long, double, etc...
 template <typename W>
 W generate_weight(W range)
 {
 	return ( rand() % range );
 }
-/* we use different method of a weight generation
- * fo float type
- */
+
+// if W is float then
+// I use different method of a weight generation
 float generate_weight(float range)
 {
 	return static_cast <float> (rand()) / static_cast <float> (range);
 }
 
-/* Constructor.
- * Generate a graph with given size, density
- * and distance weights values
- */
+// Constructor.
+// Generate a graph with given size, density
+// and distance weights values
 template <typename W>
 Graph<W>::Graph(unsigned int size, float density, W range) :
 	mSize(abs(size)),
@@ -69,8 +87,16 @@ Graph<W>::Graph(unsigned int size, float density, W range) :
 	mGraph = new W*[size];
 	mOSP = new W*[size];
 
-	cout << "lDensity " << lDensity << endl;
+	cout << endl;
+	cout << "Size: " << size << " Density: " << lDensity << "% Range: " << range << endl;
+	cout << "Initializing graph..." << endl;
 
+	testprobe(10);
+	cout << endl;
+
+	// allocate conectivity matrix
+	// and open shortest path (OSP)
+	// matrix
 	for(int i = 0; i < size; ++i)
 	{
 		mGraph[i] = new W[size];
@@ -80,6 +106,7 @@ Graph<W>::Graph(unsigned int size, float density, W range) :
 	for(int i=0; i < size; ++i)
 		for(int j=0; j < size; ++j)
 		{
+			// fill in OSP matrix with INFINITY weights
 			mOSP[i][j] = mOSP[j][i] = static_cast<W>INFINITY;
 			if (i==j)
 				mGraph[i][j] = 0; // no loops
@@ -87,11 +114,13 @@ Graph<W>::Graph(unsigned int size, float density, W range) :
 			{
 				// it is easier to use 0-100
 				// probability values than 0.0-1.0
-				mGraph[i][j] = mGraph[j][i] = (probe() < lDensity) ? 0
+				mGraph[i][j] = mGraph[j][i] = (probe() > lDensity) ? 0
 				                              : generate_weight(range);
 			}
 		}
 }
+// Destructor
+// frees memories
 template <typename W>
 Graph<W>::~Graph()
 {
@@ -101,10 +130,11 @@ Graph<W>::~Graph()
 		delete mOSP[i];
 	}
 }
+// Prints connectivity and OSP matrixies
 template <typename W>
 void Graph<W>::printGraph()
 {
-	cout << "The connectivity matrix: " << endl;
+	cout << "The connectivity matrix (the graph): " << endl;
 	for (int i=0; i < mSize; ++i)
 	{
 		for(int j=0; j < mSize; ++j)
@@ -112,7 +142,7 @@ void Graph<W>::printGraph()
 
 		cout << endl;
 	}
-	cout << "The osp matrix: " << endl;
+	cout << "The osp matrix (the open shortest paths weights): " << endl;
 	for (int i=0; i < mSize; ++i)
 	{
 		for(int j=0; j < mSize; ++j)
@@ -121,7 +151,7 @@ void Graph<W>::printGraph()
 		cout << endl;
 	}
 }
-// A utility function to find the vertex with minimum distance value, from
+// finds the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
 template <typename W>
 W Graph<W>::minDistance(W dist[], bool sptSet[])
@@ -136,12 +166,12 @@ W Graph<W>::minDistance(W dist[], bool sptSet[])
 	return min_index;
 }
 
-// A utility function to print the constructed distance array
+// prints the constructed distance array
 template <typename W>
 void Graph<W>::printSolution(W dist[], int src)
 {
 	cout << "Vertex Distance from Source " << src << endl;
-	cout << "vertex   "; 
+	cout << "vertex   ";
 	for (int i = 0; i < mSize; i++)
 		cout << i << "\t";
 	cout << endl;
@@ -151,34 +181,35 @@ void Graph<W>::printSolution(W dist[], int src)
 		cout << dist[i] << "\t";
 	cout << endl;
 }
-
+// saves the solution into OSP matrix
 template <typename W>
 void Graph<W>::saveSolution(W dist[], int src)
 {
-	cout << "Saving OSP for vertex " << src  << endl;
+	// cout << "Saving OSP for vertex " << src  << endl;
+	// use 0 instead of INFINITY for marking "no path"
 	for (int i = 0; i < mSize; i++)
-		mOSP[src][i] = dist[i];
+		mOSP[src][i] = (dist[i] == static_cast<W>INFINITY) ? 0 : dist[i];
 }
-
+// Dijkstra algorythm implementation
 #if 1
 template <typename W>
 void Graph<W>::ospDijkstra(int src)
 {
 	W dist[mSize];  // The output array.  dist[i] will hold the shortest
-					// distance from src to i
+	// distance from src to i
 
 	bool sptSet[mSize];// sptSet[i] will true if vertex i is included in shortest
-					   // path tree or shortest distance from src to i is finalized
+	// path tree or shortest distance from src to i is finalized
 
 	// Initialize all distances as INFINITE and stpSet[] as false
-	for (int i = 0; i < mSize; i++)
+	for (int i = 0; i < mSize; ++i)
 		dist[i] = static_cast<W>INFINITY, sptSet[i] = false;
 
 	// Distance of source vertex from itself is always 0
 	dist[src] = 0;
 
 	// Find shortest path for all vertices
-	for (int count = 0; count < mSize-1; count++)
+	for (int count = 0; count < mSize-1; ++count)
 	{
 		// Pick the minimum distance vertex from the set of vertices not
 		// yet processed. u is always equal to src in first iteration.
@@ -193,27 +224,70 @@ void Graph<W>::ospDijkstra(int src)
 			// Update dist[v] only if is not in sptSet, there is an edge from
 			// u to v, and total weight of path from src to  v through u is
 			// smaller than current value of dist[v]
-			if (!sptSet[v] && mGraph[u][v] && dist[u] != INFINITY
+			if (!sptSet[v] && mGraph[u][v] && dist[u] != static_cast<W>INFINITY
 			        && dist[u]+mGraph[u][v] < dist[v])
 				dist[v] = dist[u] + mGraph[u][v];
 	}
 
 	// print the constructed distance array
-	printSolution(dist, src);
+	//printSolution(dist, src);
 	saveSolution(dist, src);
 }
 #endif
-int main( void )
+# if 1
+// Calculates OSP averages for given number of paths
+template <typename W>
+W Graph<W>::ospAverage(unsigned int num)
 {
-	Graph<int> myGraph = Graph<int>(4, 0.5, 10);
-#if 1
-	for (int i=0; i < myGraph.getSize(); ++i)
-		myGraph.ospDijkstra( i ); // calculate all the OSPs
+	int counter = 0; // paths counter
+	W weight = static_cast<W> (0);
+
+	for (int i=0; i < mSize; ++i)
+	{
+		for (int j=0; j < mSize; ++j)
+		{
+			if (mOSP[i][j] > 0)
+				weight += mOSP[i][j], counter++;
+			if (counter == num)
+				break;
+		}
+		if (counter == num)
+			break;
+	}
+
+	if (counter != num)
+	{
+		cout << endl << "Too few paths in a graph" << endl;
+		return 0;
+	}
+	else
+		return weight/static_cast<W>(counter);
+}
 #endif
 
-	myGraph.printGraph();
+int main( void )
+{
+	/* graph initialization constants */
+	const int size = 50;
+	const float density = 0.4;
+	const int range = 10;
+	const int montecarloNum = 50;
+
+	// make a graph with it's constructor
+	Graph<int> myGraph = Graph<int>(size, density, range);
+
+	// calculate all the OSPs in the graph
+	for (int i=0; i < myGraph.getSize(); ++i)
+		myGraph.ospDijkstra( i );
+
+	// print connectivity matrix ans OSP matrix
+	//myGraph.printGraph();
+
+	// calculate and print average OSP length
+	cout << "Average path length for " << montecarloNum << \
+	     " paths is:"	<< myGraph.ospAverage(50) << endl;
+
 	return 0;
 }
-
 
 
